ccfc9be3b4f4e13bfea1dc2177e7ab7e50131aae
hirangi <hira13@gmail.com>
2015-01-29 08:02:02 +0000

    Addd part 2 and 3 of exercise of 1

--- exercise1.py
+++ exercise1.py
@@ -4,3 +4,17 @@ How many times will 'hello' be printed?
 for i in range(3, 17):
     print 'hello'
 # prints 14 times hello 
+
+# 2)
+for j in range(12):
+    if j % 3 == 0:
+       print 'hello'
+#prints hello 4 times
+
+# 3)
+for j in range(15):
+	if j % 5 == 3:
+		print 'hello'
+	elif j % 4 == 3:
+		print 'hello'
+#prints hello 5 times 
